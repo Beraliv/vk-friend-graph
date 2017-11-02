c985da7a3c7f2016e882993a064e775e9fd722ec
beraliv <beraliv.spb@gmail.com>
2017-11-02 22:11:10 +0300

    [vue-vk-friend-graph@0.0.1] stopped by 'did you register the component correctly?'

--- poi.config.js
+++ poi.config.js
@@ -6,10 +6,40 @@ module.exports = {
     'src/polyfills.js',
     'src/index.js'
   ],
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['.js', '.vue']
+  },
   html: {
     title: pkg.productName,
     description: pkg.description,
     template: path.join(__dirname, 'index.ejs')
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.vue$/,
+        enforce: 'pre',
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /\.min.js$/
+        ],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
   postcss: {
     plugins: [
