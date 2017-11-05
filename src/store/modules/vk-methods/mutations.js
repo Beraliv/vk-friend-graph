c8c9fcf343988b60f3f6caeb7097b6edb254033d
beraliv <beraliv.spb@gmail.com>
2017-11-05 22:00:35 +0300

    [vue-vk-friend-graph@0.1.0] worked for most of all last browsers (ie, edge, firefox, chrome)

--- src/store/modules/vk-methods/mutations.js
+++ src/store/modules/vk-methods/mutations.js
@@ -1,9 +1,38 @@
 import * as types from './types';
 
+import {
+  combineGetters
+} from '../../../helpers/functions';
+
 let deactivatedUserIds;
 const notDeactivated = users => {
-  deactivatedUserIds = users.filter(user => user.deactivated).map(user => user.user_id);
-  return users.filter(user => !user.deactivated);
+  deactivatedUserIds = combineGetters(
+    users,
+    users => users.filter(user => user.deactivated),
+    notDeactivatedUsers => notDeactivatedUsers.map(user => user.user_id)
+  ) || [];
+  return combineGetters(
+    users,
+    users => users.filter(user => !user.deactivated)
+  ) || [];
+};
+
+const nonRepeatingConnections = connections => {
+  let result = new Map();
+  for (let { common_friends, id: sid } of connections) {
+    for (let tid of common_friends) {
+      if (deactivatedUserIds.findIndex(id => id === tid) < 0) {
+        let less = +sid < +tid;
+        let value = less ? { sid, tid } : { sid: tid, tid: sid };
+        if (!result.has(value.sid)) {
+          result.set(value.sid, new Set());
+        }
+        let set = result.get(value.sid);
+        set.add(value.tid);
+      }
+    }
+  }
+  return result;
 };
 
 const mutations = {
@@ -39,27 +68,19 @@ const mutations = {
 
   [types.VK_GET_MUTUAL_FRIENDS_REQUEST](state) {},
 
-  [types.VK_GET_MUTUAL_FRIENDS_SUCCESS](state, { connections }) {
-    let allNotRepeatingConnections = new Map();
-    for (let { common_friends, id: sid } of connections) {
-      for (let tid of common_friends) {
-        if (deactivatedUserIds.findIndex(id => id === tid) < 0) {
-          let less = +sid < +tid;
-          let value = less ? { sid, tid } : { sid: tid, tid: sid };
-          if (!allNotRepeatingConnections.has(value.sid)) {
-            allNotRepeatingConnections.set(value.sid, new Set());
-          }
-          let set = allNotRepeatingConnections.get(value.sid);
-          set.add(value.tid);
-        }
-      }
-    }
+  [types.VK_GET_MUTUAL_FRIENDS_SUCCESS](state, { connections } = {}) {
+    const allNotRepeatingConnections = combineGetters(
+      connections,
+      connections => nonRepeatingConnections(connections),
+      connections => [...connections]
+    );
+
     state.connections = [
       ...state.connections,
-      ...[...allNotRepeatingConnections].reduce((arr, entry) => {
-        const entries = [...entry[1]].map(id => ({
-          sid: entry[0],
-          tid: id
+      ...allNotRepeatingConnections.reduce((arr, [sid, tids]) => {
+        const entries = [...tids].map(tid => ({
+          sid,
+          tid
         }));
         return [
           ...arr,
